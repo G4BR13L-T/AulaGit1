249644afb2ac67bf4adf87712f6b543251058d14
G4BR13L-T <gabrieltoldo81@gmail.com>
2024-10-18 17:12:57 -0300

    só sei que de nada sei...

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! {|");
 
         for (int i = 0; i < 5; i++){
             System.out.println(i + 1 + "Olá");
