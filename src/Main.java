ec813f869f8ddae74739a0fe8428fb15c7f7fd1c
G4BR13L-T <gabrieltoldo81@gmail.com>
2024-10-18 17:12:10 -0300

    só sei que nada sei...

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! ☺♪♀♫♂◙");
+        System.out.println("Hello World!");
 
         for (int i = 0; i < 5; i++){
-            System.out.println(i + 1 + "Olá");
+            System.out.println(i + 1 + "Ola");
         }
     }
 }
